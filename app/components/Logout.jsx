04f98f7d61a755c698dc638d591fe5edebdbc6d3
Chloe Hwang <c.chloeh@gmail.com>
2017-03-27 21:59:29 -0400

    fixed issue with logout not working

--- app/components/Logout.jsx
+++ app/components/Logout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { LinkContainer } from 'react-router-bootstrap'
 import { Nav, NavItem } from 'react-bootstrap'
 
-export default function() {
+
+export default function({ handleLogout }) {
   return (
       <Nav pullRight>
-        <NavItem eventKey={2} onClick={this.props.logout}>LOGOUT</NavItem>
+        <NavItem eventKey={2} onClick={handleLogout}>LOGOUT</NavItem>
       </Nav>
     );
 }
